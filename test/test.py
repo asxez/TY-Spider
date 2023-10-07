9b69881b04d33fbf3acbf71891160137e77715ff
asxe <2973918177@qq.com>
2023-10-07 22:06:42 +0800

    测试

--- test/test.py
+++ test/test.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 data = {
-    'q':'中医'
+    'q': '前端开发'
 }
-res = requests.post('http://127.0.0.1:1314/search',data=data).text
-print(json.loads(res).get('response'))
+res = requests.post('http://127.0.0.1:1314/search/', data=data)
+print(res.text)
