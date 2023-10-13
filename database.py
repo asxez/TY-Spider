ac7cd16187685f5ae15cf64a5516d0e342362ffc
asxe <2973918177@qq.com>
2023-10-13 13:59:10 +0800

    新增数据库连接

---
+++ database.py
@@ -0,0 +1,24 @@
+import pymongo
+from loguru import logger
+from config import mongodb_host, mongodb_port
+
+
+class MongoDB:
+    def __init__(self):
+        self.client = None
+        self.db = None
+        self.col = None
+        self.closed = False
+
+    def __enter__(self):
+        self.client = pymongo.MongoClient(host=mongodb_host, port=mongodb_port)
+        self.db = self.client['datas']
+        self.col = self.db['sites']
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if not self.closed:
+            if exc_type is not None:
+                logger.error(f'出现错误：{exc_type}-{exc_val}-{exc_tb}')
+            self.client.close()
+            self.closed = True
