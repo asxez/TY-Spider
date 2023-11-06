cbfa62d0263bb7159c804e6233a2d7ed1413bf72
asxe <2973918177@qq.com>
2023-11-06 22:33:48 +0800

    使用双引号进行保存

--- mongodb.py
+++ mongodb.py
@@ -12,7 +12,7 @@ def cost_time(func: Callable) -> Callable:
     def fun(*args, **kwargs):
         t = time.perf_counter()
         result = func(*args, **kwargs)
-        logger.info(f'{func.__name__} 耗时：{time.perf_counter() - t:.8f}s')
+        logger.info(f"{func.__name__} 耗时：{time.perf_counter() - t:.8f}s")
         return result
 
     return fun
@@ -21,8 +21,8 @@ def cost_time(func: Callable) -> Callable:
 @cost_time
 def save_data(datas: list, col: MongoClient) -> None:
     if len(datas) == 0:
-        logger.info('保存数据时所接受的列表为空')
-        return
+        logger.info("保存数据时所接受的列表为空")
+        return None
     col.insert_many(datas)
 
 
@@ -30,19 +30,19 @@ def save_data(datas: list, col: MongoClient) -> None:
 def del_repeat(col: MongoClient) -> None:
     pipeline = [
         {
-            '$group': {
-                '_id': '$href',  # 根据href字段去重
-                'doc_id': {'$first': '$_id'},  # 保留第一个文档的_id字段
-                'doc': {'$first': '$$ROOT'}  # 保留第一个完整文档
+            "$group": {
+                "_id": "$href",  # 根据href字段去重
+                "doc_id": {"$first": "$_id"},  # 保留第一个文档的_id字段
+                "doc": {"$first": "$$ROOT"}  # 保留第一个完整文档
             }
         },
         {
-            '$replaceRoot': {
-                'newRoot': '$doc'  # 使用$replaceRoot将文档重新变回根文档，保留原始字段
+            "$replaceRoot": {
+                "newRoot": "$doc"  # 使用$replaceRoot将文档重新变回根文档，保留原始字段
             }
         },
         {
-            '$out': 'sites'  # 将去重后的结果写入新集合，此处为覆盖原集合
+            "$out": "sites"  # 将去重后的结果写入新集合，此处为覆盖原集合
         }
     ]
     col.aggregate(pipeline=pipeline)
@@ -59,7 +59,7 @@ def delete_keywords_and_description(col: MongoClient, description: str = "", wor
         ]
     }
     result = col.delete_many(query)
-    logger.info(f'删除了 {result.deleted_count} 条数据')
+    logger.info(f"删除了 {result.deleted_count} 条数据")
 
 
 @cost_time
@@ -71,17 +71,17 @@ def search_data(text: str, col: MongoClient) -> Cursor[Mapping[str, Any]]:
             {"title": {"$regex": text, "$options": "i"}}
         ]
     }
-    results = col.find(query)
+    results = col.find(query, projection={"_id": 0})
     return results
 
 
 @cost_time
 def creat_index(col: MongoClient) -> None:
-    index = [('description', ASCENDING), ('word', ASCENDING), ('title', ASCENDING)]
+    index = [("description", ASCENDING), ("word", ASCENDING), ("title", ASCENDING)]
     col.create_index(index)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     with MongoDB() as db:
         del_repeat(db.col)
     # threading.Thread(target=get_response_from_bfs).start()
