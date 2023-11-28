99362c3605d732dc24fd5c56c6407a9144b4b5a9
asxe <2973918177@qq.com>
2023-11-28 12:30:07 +0800

    定时任务，logo

---
+++ schedule.py
@@ -0,0 +1,29 @@
+from typing import Callable
+from datetime import date
+import time
+
+from loguru import logger
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.cron import CronTrigger
+
+
+def logging(func: Callable) -> Callable:
+    def wrapper(*args, **kwargs):
+        logger.info(f"{func.__name__}: {date.today()}")
+        return func(*args, **kwargs)
+
+    return wrapper
+
+
+def schedule(func: Callable, args: tuple, hour: int, minute: int) -> None:
+    scheduler = BackgroundScheduler()
+    wrapped_func = logging(func)
+
+    scheduler.add_job(wrapped_func, trigger=CronTrigger(hour=hour, minute=minute), args=args)
+    scheduler.start()
+
+    try:
+        while True:
+            time.sleep(1)
+    except (KeyboardInterrupt, SystemExit):
+        scheduler.shutdown()
