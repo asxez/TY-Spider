cf6a91202863a812203a5d779aa80a4f7dc37994
asxe <2973918177@qq.com>
2023-12-03 11:03:19 +0800

    语言检测，内置，剔除无用代码

--- utils.py
+++ utils.py
@@ -1,7 +1,9 @@
 from time import perf_counter
-from typing import Callable
+from typing import Callable, Any
 
 from loguru import logger
+
+_lang_model = None
 
 
 def cost_time(func: Callable) -> Callable:
@@ -12,3 +14,18 @@ def cost_time(func: Callable) -> Callable:
         return result
 
     return fun
+
+
+def check_lang(s: str) -> tuple[Any, Any]:
+    global _lang_model
+    if not _lang_model:
+        import fasttext
+        try:
+            fasttext.FastText.eprint = lambda *args, **kwargs: None
+        except Exception:
+            pass
+        _lang_model = fasttext.load_model('lid.176.ftz')
+    res = _lang_model.predict(s.replace('\n', ''))
+    lang = res[0][0][9:]
+    like = res[1][0]
+    return lang, like
